1598496e446aeffb40b559862b0075f6ddead050
eunabae <dmsdk0940@naver.com>
2021-04-07 00:08:47 +0900

    신규아이디추천

--- Programmers/level1/신규아이디추천.py
+++ Programmers/level1/신규아이디추천.py
@@ -21,7 +21,7 @@ def solution(new_id):
 
     #6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
     #만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-
+    
     if(len(answer)>=16):
         answer=answer[0:15].rstrip('.')
 
